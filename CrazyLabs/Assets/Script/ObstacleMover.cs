63fe73dbe290f1603af7c7d162e278d044ec6468
Lpmus <86368317+Lpmus@users.noreply.github.com>
2022-06-15 11:04:18 +0000

    Added moving obstacles

---
+++ CrazyLabs/Assets/Script/ObstacleMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+
+    public GameObject player;
+    private void Awake()
+    {
+       // player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject==player)
+        {
+            player.transform.parent = transform;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+
+        }
+
+    }
+
+}
