8f191c81a7f1b53d7cdb4484d56d49e114164e0e
Lpmus <86368317+Lpmus@users.noreply.github.com>
2022-06-28 09:14:56 +0000

    Hen Win Movement First stage

--- CrazyLabs/Assets/Script/MoveToFinish.cs
+++ CrazyLabs/Assets/Script/MoveToFinish.cs
@@ -7,12 +7,13 @@ public class MoveToFinish : MonoBehaviour
 {
 
     public EnemyAiTutorial thisEnemy;
-    public GameObject coopLocation;
+    public Transform henLocation;
+  
     // Start is called before the first frame update
     void Start()
     {
         thisEnemy = GetComponent<EnemyAiTutorial>();
-        coopLocation = GameObject.FindGameObjectWithTag("HenCoop");
+        henLocation = GameObject.Find("HenLocation").transform;
     }
 
     // Update is called once per frame
@@ -21,10 +22,10 @@ public class MoveToFinish : MonoBehaviour
         if (GameManager.instance.gameWon ==true)
         {
             thisEnemy.enabled = false;
-            transform.LookAt(coopLocation.transform.position);
-            transform.position = Vector3.MoveTowards(transform.position, coopLocation.transform.position, 0.08f);
-           
-            if (Vector3.Distance(transform.position,coopLocation.transform.position) < 5.5f)
+                transform.LookAt(henLocation.position);
+                transform.position = Vector3.MoveTowards(transform.position, henLocation.position, 0.08f); 
+
+            if (Vector3.Distance(transform.position,henLocation.position) < 2.0f)
                {
                 Destroy(gameObject);
             }
